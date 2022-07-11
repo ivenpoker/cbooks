daca1a1e831be8f9a1114d0abf623178740f0ef3
ivenpoker <ivenpoker@gmail.com>
2022-07-11 23:04:08 +0100

    main | platforms | w3resource | basic declaration and expressions | exercise 5.

---
+++ platforms/w3resource/sections/basicdeclarationsandexpressions/exercise5/main.c
@@ -0,0 +1,34 @@
+//
+// Created by ivenpoker on 7/11/22.
+//
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+int main(int argc, string_pt argv) {
+
+    double height = 0.0;
+    double weight = 0.0;
+
+    fprintf(stdout, "Enter height in inches: ");
+    fscanf(stdin, "%lf", &height);
+
+    fprintf(stdout, "Enter weight in inches: ");
+    fscanf(stdin, "%lf", &weight);
+
+    fprintf(stdout, "\nResults...");
+    fprintf(stdout, "\nArea: %.2f", areaOf(height, weight));
+    fprintf(stdout, "\nPerimeter: %.2f\n", perimeterOf(height, weight));
+
+    exit(EXIT_SUCCESS);
+}
+
+double areaOf(double height, double width) {
+    return height * width;
+}
+
+double perimeterOf(double height, double width) {
+    return 2 * (height + width);
+}
---
+++ platforms/w3resource/sections/basicdeclarationsandexpressions/exercise5/main.h
@@ -0,0 +1,14 @@
+//
+// Created by ivenpoker on 7/11/22.
+//
+
+#ifndef CBOOKS_MAIN_EXERCISE_5_H
+#define CBOOKS_MAIN_EXERCISE_5_H
+
+typedef char *string_t;
+typedef string_t *string_pt;
+
+double areaOf(double height, double width);
+double perimeterOf(double height, double width);
+
+#endif //CBOOKS_MAIN_EXERCISE_5_H
