c5669aa44fd15cfe76da5c1fe5347037e1e2030b
ivenpoker <ivenpoker@gmail.com>
2022-07-11 23:47:59 +0100

    main | platforms | w3resource | basic declaration and expressions | exercise 7.

---
+++ platforms/w3resource/sections/basicdeclarationsandexpressions/exercise7/solution/main.h
@@ -0,0 +1,11 @@
+//
+// Created by ivenpoker on 7/11/22.
+//
+
+#ifndef CBOOKS_MAIN_EXERCISE_7_H
+#define CBOOKS_MAIN_EXERCISE_7_H
+
+typedef char *string_t;
+typedef string_t* string_pt;
+
+#endif //CBOOKS_MAIN_EXERCISE_7_H
