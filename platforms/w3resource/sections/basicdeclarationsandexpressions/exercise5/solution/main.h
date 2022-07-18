639203a6fecb1b7a1a76432262964026ab7c829c
ivenpoker <ivenpoker@gmail.com>
2022-07-18 10:33:12 +0100

    main | platforms | w3resource | basic declaration and expressions | exercise 5 (update).

--- platforms/w3resource/sections/basicdeclarationsandexpressions/exercise5/solution/main.h
+++ platforms/w3resource/sections/basicdeclarationsandexpressions/exercise5/solution/main.h
@@ -2,8 +2,8 @@
 // Created by ivenpoker on 7/11/22.
 //
 
-#ifndef CBOOKS_MAIN_EXERCISE_5_H
-#define CBOOKS_MAIN_EXERCISE_5_H
+#ifndef PLATFORM_W3RESOURCE_BASIC_DECLARATIONS_AND_EXPRESSIONS_EXERCISE_5_H
+#define PLATFORM_W3RESOURCE_BASIC_DECLARATIONS_AND_EXPRESSIONS_EXERCISE_5_H
 
 typedef char *string_t;
 typedef string_t *string_pt;
@@ -11,4 +11,4 @@ typedef string_t *string_pt;
 double areaOf(double height, double width);
 double perimeterOf(double height, double width);
 
-#endif //CBOOKS_MAIN_EXERCISE_5_H
+#endif //PLATFORM_W3RESOURCE_BASIC_DECLARATIONS_AND_EXPRESSIONS_EXERCISE_5_H
