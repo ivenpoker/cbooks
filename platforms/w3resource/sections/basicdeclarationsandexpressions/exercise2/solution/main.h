9c36b460525f8a61a23486d7f63a9bb533252b7e
ivenpoker <ivenpoker@gmail.com>
2022-07-18 10:30:11 +0100

    main | platforms | w3resource | basic declaration and expressions | exercise 2 (update).

--- platforms/w3resource/sections/basicdeclarationsandexpressions/exercise2/solution/main.h
+++ platforms/w3resource/sections/basicdeclarationsandexpressions/exercise2/solution/main.h
@@ -2,10 +2,10 @@
 // Created by ivenpoker on 7/11/22.
 //
 
-#ifndef CBOOKS_MAIN_EXERCISE_2_H
-#define CBOOKS_MAIN_EXERCISE_2_H
+#ifndef PLATFORM_W3RESOURCE_MAIN_EXERCISE_2_H
+#define PLATFORM_W3RESOURCE_MAIN_EXERCISE_2_H
 
 typedef char *string_t;
 typedef string_t *string_pt;
 
-#endif //CBOOKS_MAIN_EXERCISE_2_H
+#endif //PLATFORM_W3RESOURCE_MAIN_EXERCISE_2_H
